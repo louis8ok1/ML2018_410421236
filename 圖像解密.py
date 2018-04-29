a6e99a84b91aa375a2b5128850612ad8da145ae3
louis8ok1 <louis8ok1@gmail.com>
2018-04-29 18:52:09 +0800

    有成功跑出權重，但圖片還出不來

--- 圖像解密.py
+++ 圖像解密.py
@@ -4,19 +4,46 @@ Created on Sun Apr 22 22:57:32 2018
 
 @author: louis
 """
-import keras
 import numpy as np
+from PIL import Image
+import math
 
-#load_data
+#---------------------------------------------------------#
+#load_data and preparing
 
-test=np.loadtxt("G:\學校課堂資料\東華大三下\機器學習\ML_2018_410421236\data\eprime.txt")
+k1 = Image.open("G:\學校課堂資料\東華大三下\機器學習\ML_2018_410421236\data\key1.png") 
+k2 = Image.open("G:\學校課堂資料\東華大三下\機器學習\ML_2018_410421236\data\key2.png") 
+I  = Image.open("G:\學校課堂資料\東華大三下\機器學習\ML_2018_410421236\data\I.png")
+E  = Image.open("G:\學校課堂資料\東華大三下\機器學習\ML_2018_410421236\data\Eprime.png")
+#k1.save("G:\學校課堂資料\東華大三下\機器學習\ML_2018_410421236\data\Check_k1_gray.png")
+
+k1 = list(k1.getdata())
+k2 = list(k2.getdata())
+I  = list(I.getdata())
+E  = list(E.getdata())
+
+print(I)
+
+learning_rate = 0.00001
+Epoch = 1
+
+w=np.random.rand(3)
+
+print (w)
 
 
 
-test.flatten("F")
+#=============================================================#
 
-print(test)
-test=np.savetxt("test.txt",test)
+
+
+
+
+
+
+
+
+
 
 
 
