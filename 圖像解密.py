4b71e1bbdab80503211240bf8ed9978fb7289a95
louis8ok1 <louis8ok1@gmail.com>
2018-04-30 12:02:21 +0800

    做了一些小更正

--- 圖像解密.py
+++ 圖像解密.py
@@ -32,7 +32,7 @@ I = imageio.imread("G:\學校課堂資料\東華大三下\機器學習\ML_2018_4
 Epoch = 1
 
 weight = np.random.rand(3)#讓權重有隨機的初始值
-
+#Weight[0],weight[1],weight[2]
 
 learning_rate=0.00001#當我嘗試調高學習率時，直接不能跑
 
@@ -43,7 +43,8 @@ L = 400#data 的length
 
 W = 300#data 的width
 
-print(">Random Weight:",weight)#看一下還沒訓練的權重
+print(">初始 Random Weight:",weight)#看一下還沒訓練的隨機權重
+
 
 
 #==================================================================================================#
@@ -54,29 +55,31 @@ while (Epoch==1 or Epoch<MaxIterLimit and np.any(np.absolute(weight[1]-weight[0]
    
     for i in range(W):
         
-    
-        
         temp0=0
         
         for j in range(L):
             temp1 = weight[0]*k1[i][j]+ weight[1]*k2[i][j]+weight[2]*I[i][j]
+            #被解密的照片的第[i][j]位置
             temp0 = E[i][j]-temp1 
-            
+            #變異量
             weight[0]=weight[0]+learning_rate*temp0*k1[i][j]
             weight[1]=weight[1]+learning_rate*temp0*k2[i][j]
             weight[2]=weight[2]+learning_rate*temp0*I[i][j]
-           
-    print(">Epoch = ",Epoch,"and Difference is:",temp0)
+            if(j%300==0 and i%300==0):
+                print(weight)
+    print(">Epoch = ",Epoch)
+    print(">Difference is:",temp0)
     
     Epoch += 1
     
     if Epoch >=10:
-          
+                 
+           
            ans_picture= (Eprime - (weight[0]*k1)-(weight[1]*k2))/weight[2]
            
            plt.imshow(ans_picture,cmap="gray")
            plt.show()
-           print(weight)
+           print("調整過後的weight=",weight)
         
           
            
